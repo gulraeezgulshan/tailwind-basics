fd37a3546f8f1a99212d49fa0b1f11aa8e7ec8df
Gulraeez Gulshan <gulraeezgulshan@gmail.com>
2024-03-15 14:11:47 +0500

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,12 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        'gmail' : {
+          '50': '#fdf4f3',
+          '100' : '#fce6e4',
+          '500' : '#de493d'
+        }
       },
     },
   },
